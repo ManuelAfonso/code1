e2562446912026710487bde3b15277f8975db244
Manuel Afonso <manuel.afonso@criticalsoftware.com>
2018-08-13 10:51:43 +0100

    cleaning

--- Common/Persistance/IObjectSerializer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Code1.Common.Persistance
-{
-    public interface IObjectSerializer
-    {
-        T DeserializeObject<T>(string text);
-        string SerializeObject(object value);
-    }
-}
